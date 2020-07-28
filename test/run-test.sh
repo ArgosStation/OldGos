5cf8e432710b6adfd5173c5dff37a25fc0bc2697
RustingWithYou <cam.olssen@gmail.com>
2020-07-28 14:55:18 +1200

    renames dme

--- test/run-test.sh
+++ test/run-test.sh
@@ -228,9 +228,9 @@ function run_byond_tests {
         ./install-byond.sh || exit 1
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
-    run_test_ci "check globals build" "python3 tools/GenerateGlobalVarAccess/gen_globals.py baystation12.dme code/_helpers/global_access.dm"
+    run_test_ci "check globals build" "python3 tools/GenerateGlobalVarAccess/gen_globals.py argosstation.dme code/_helpers/global_access.dm"
     run_test "check globals unchanged" "md5sum -c - <<< 'a2f71ab15f3011ec1f47876dc24b2855 *code/_helpers/global_access.dm'"
-    run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH baystation12.dme"
+    run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH argosstation.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
     run_test "run unit tests" "DreamDaemon baystation12.dmb -invisible -trusted -core 2>&1 | tee log.txt"
     run_test "check tests passed" "grep 'All Unit Tests Passed' log.txt"
